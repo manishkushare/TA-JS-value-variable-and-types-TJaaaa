2fd2a57e0fec5fb5997807e219e0f858838f8c9b
Manish Kushare <manishkushare9@gmail.com>
2021-01-04 20:36:03 +0530

    solve repo

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,21 +6,64 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt("Please enter your name");
+alert(user);
+let userName = user;
+console.log(userName, user);
+let age = 22 , isLearning = true;
+user = "John";
+console.log(user);
+console.log(userName);
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+let numA = +prompt("Please enter first number");
+let numB = +prompt("please enter second number");
+let sum = numA + numB;
+alert(sum);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++){
+  if(j % 2 !== 0) continue;
+ console.log(j);
+}    
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for (let j = 9; j >=1 ; j--){
+  if(j % 2 == 0) continue;
+ console.log(j);
+}    
+
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
 
-// 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+let i = 5;
+let concat = "";
+while(i >= 0){
+ concat += String(i);
+ i--
+}
+console.log(concat);
+
+
+
+// Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+
+let i = 10;
+let concat = "";
+while(i >= 0) {
+    if(i % 2 == 0) {
+        concat += i;
+    }
+    i--;
+}
+console.log(concat);
 
 // Comparisoin
 
@@ -36,3 +79,16 @@ Example:
 "hello", -21; // false
 
 */
+let num1 = prompt("Enter afirst number");
+let num2 = prompt("enter second number");
+
+if(num1 == num2) {
+    alert(true);
+
+}
+else if(num1 !== num2) {
+    alert(false);
+}
+else {
+    alert("Enter a valid input");
+}
